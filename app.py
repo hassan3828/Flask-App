4763ca18a8d9b669f3a84be50f2cc80ee06bd9f5
hassan3828 <hassan38281@gmail.com>
2024-08-09 18:34:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+
+def login():
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+
+        password = request.form['password']
+
+
+
+        # Check if the username and password are correct
+
+        if username == 'HASSAN-RAJPUT' and password == 'HELL-INXIDE':
+
+            # Redirect to the specified link if login is successful
+
+            return redirect('https://creepy-jillayne-hassanmaster-56070bbd.koyeb.app')
+
+        else:
+
+            return 'Invalid username or password. Please try again.'
+            return render_template('index.html')
 
 
 if __name__ == '__main__':
